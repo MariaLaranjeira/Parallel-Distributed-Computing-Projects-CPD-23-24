f8d46923b23e7d4a7cb5191fc21b7d450240edea
Maria Laranjeira <up202004453@up.pt>
2024-03-10 17:00:41 +0000

    Added Parallel Line Multiplication

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <cstdlib>
 #include <papi.h>
+#include <omp.h>
 
 using namespace std;
 
@@ -104,6 +105,57 @@ void OnMultLine(int m_ar, int m_br)
 
     Time2 = clock();
     sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+    cout << st;
+
+
+    // display 10 elements of the result matrix tto verify correctness
+    cout << "Result matrix: " << endl;
+    for(i=0; i<1; i++)
+    {    for(j=0; j<min(10,m_br); j++)
+            cout << phc[j] << " ";
+    }
+    cout << endl;
+
+    free(pha);
+    free(phb);
+	free(phc);
+}
+
+// add code here for line x line matriz multiplication
+void OnMultLineCores(int m_ar, int m_br)
+{
+    double Time1, Time2;
+    
+    char st[100];
+	double temp;
+    int i, j, k;
+
+    double *pha, *phb, *phc;
+    
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+    phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+    for(i=0; i<m_ar; i++)
+        for(j=0; j<m_ar; j++)
+            pha[i*m_ar + j] = (double)1.0;
+
+    for(i=0; i<m_br; i++)
+        for(j=0; j<m_br; j++)
+            phb[i*m_br + j] = (double)(i+1);
+
+
+    Time1 = omp_get_wtime();
+
+	#pragma omp parallel for private(i, j, k) shared(pha, phb, phc)
+    for(i=0; i<m_ar; i++)
+        for( j=0; j<m_ar; j++ )
+        	for( k=0; k<m_br; k++)
+                phc[i*m_ar + k] += pha[i*m_ar + j] * phb[j*m_br + k];
+            
+
+    Time2 =  omp_get_wtime();
+    sprintf(st, "Time: %3.3f seconds\n", Time2 - Time1);
     cout << st;
 
 
@@ -227,6 +279,7 @@ int main (int argc, char *argv[])
 		cout << "1. Multiplication" << endl;
 		cout << "2. Line Multiplication" << endl;
 		cout << "3. Block Multiplication" << endl;
+		cout << "4. Parallel Line Multiplication" << endl;
 		cout << "0. Exit" << endl;
 		cout << "Selection?: ";
 		cin >>op;
@@ -253,6 +306,9 @@ int main (int argc, char *argv[])
 				cin >> blockSize;
 				OnMultBlock(lin, col, blockSize);  
 				break;
+			case 4:
+				OnMultLineCores(lin, col);  
+				break;		
 		}
 
   		ret = PAPI_stop(EventSet, values);
--- assign1/src/run.sh
+++ assign1/src/run.sh
@@ -1,4 +1,4 @@
-g++ -O2 matrixproduct.cpp -o matrixproduct -lpapi 
+g++ -O2 matrixproduct.cpp -o matrixproduct -lpapi -fopenmp
 javac MatrixProduct.java
 
 # Run the C++ program
